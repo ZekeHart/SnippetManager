8ac259ca031f6a51b14f5ac4bda6698d3d0e70c5
dzordich <dzordich@gmail.com>
2019-07-16 13:49:17 -0400

    created models & registered with admin site

--- snippet_manager/core/admin.py
+++ snippet_manager/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from core.models import Language, Snippet
 # Register your models here.
+
+admin.site.register(Language)
+
+@admin.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ('user', 'language', 'title')
--- snippet_manager/core/models.py
+++ snippet_manager/core/models.py
@@ -1,3 +1,31 @@
+from datetime import date
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Language (models.Model):
+    name = models.CharField(max_length=50, help_text="The language this snippet is written in.")
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Snippet (models.Model):
+    title = models.CharField(max_length=100, help_text="Brief title for the snippet.")
+    description = models.CharField(max_length=500, help_text="Brief description of the function of the snippet")
+    language = models.ForeignKey("Language", on_delete=models.SET_NULL, null=True)
+    date = models.DateField(default=date.today)
+    code = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def get_absolute_url(self):
+        return reverse('snippet', args=[str(self.pk)])
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        ordering = ['-date']
+
