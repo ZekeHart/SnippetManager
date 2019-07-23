299383d1dc43379d40eb7062ba7d2840816ff104
dzordich <dzordich@gmail.com>
2019-07-22 20:36:06 -0400

    changed language form field to dropdown for syntax highlighting, fixed language field copying

--- snippet_manager/core/serializers.py
+++ snippet_manager/core/serializers.py
@@ -3,10 +3,14 @@ from core.models import Snippet
 
 class SnippetSerializer(serializers.ModelSerializer):
     num_times_copied = serializers.SerializerMethodField()
+    user_username = serializers.SerializerMethodField()
 
     class Meta:
         model = Snippet
-        fields = ['language', 'title', 'date', 'code', 'description', 'user', 'original', 'pk', 'num_times_copied']
+        fields = ['language', 'title', 'date', 'code', 'description', 'user', 'original', 'pk', 'num_times_copied', 'user_username']
 
     def get_num_times_copied(self, obj):
         return obj.copies.count()
+
+    def get_user_username(self, obj):
+        return obj.user.get_username()
--- snippet_manager/core/static/compiled.js
+++ snippet_manager/core/static/compiled.js
@@ -40,7 +40,7 @@ function displayResults(key) {
     <div id="snippet${ key.pk}" class="card bg-transparent shadow-sm">
             <p class="card-header snippet-title"><span class="font-weight-bold">${key.title} </span></p>
             <div class="card-body">
-            <p class="card-text">${key.user} | Added on: ${key.date} | Copied ${key.num_times_copied} times</p>
+            <p class="card-text">${key.user_username} | Added on: ${key.date} | Copied ${key.num_times_copied} times</p>
             <p class="card-text text-secondary">${key.description} </p>
     <div class='code-toolbar'>	
         <pre class='line-numbers language-${key.language.toLowerCase()}'><code class='language-${key.language.toLowerCase()}'>${escapeHtml(key.code)}</code></pre>
@@ -50,7 +50,7 @@ function displayResults(key) {
     console.log('copyUser:', copyUser)
     if (document.querySelector('#loggedIn')) {
         resultsDiv.innerHTML += `<div class="alert-primary" id="copySuccess${key.pk}"></div><div class="alert-danger" id="deleteSuccess${key.pk}"></div>
-<button class="copyButton btn btn-primary mx-auto snippetCopyButton${key.pk}" data-pk="${key.pk}" data-title="${key.title}" data-language="${key.language.pk}" data-description="${key.description}" data-code="${key.code}">Add to your Library</button>`
+<button class="copyButton btn btn-primary mx-auto snippetCopyButton${key.pk}" data-pk="${key.pk}" data-title="${key.title}" data-language="${key.language}" data-description="${key.description}" data-code="${key.code}">Add to your Library</button>`
         if (key.user == copyUser) {
             resultsDiv.innerHTML += `<button class="deleteButton btn btn-danger snippetDeleteButton${key.pk}" data-pk="${key.pk}">Delete</button>`
         }
@@ -104,7 +104,9 @@ searchInput.addEventListener('input', function () {
 
 let editorLangSelect = document.querySelector("#id_language");
 if (editorLangSelect) {
+    console.log('poop')
     editorLangSelect.addEventListener("change", function () {
+        console.log('woooooooooooooooooooo')
         code_codemirror.setOption("mode", editorLangSelect.value.toLowerCase())
     })
 }
